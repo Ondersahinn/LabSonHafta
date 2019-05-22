966ef9e26ae9b45425c522954f87a4f8d2249803
Ondersahinn <46032537+Ondersahinn@users.noreply.github.com>
2019-05-22 18:19:50 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :yazars, :path=>"yazarlar"
   resources :kitaps, :path=>"kitaplar"
-  root "kitaps#index"
+ 
   resources :kitaps, :path =>"kitaplar" do
     put "like", to: "kitaps#like"
     end
